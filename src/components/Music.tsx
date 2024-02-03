ec2d3f83e15c5082ed2721a283f7ffdc13b36026
dongolheart <90121300+dongolheart@users.noreply.github.com>
2024-02-03 16:43:46 +0000

    Add files via upload

---
+++ src/components/Music.tsx
@@ -0,0 +1,23 @@
+export default function Music() {
+    return (
+      <section
+        className="bg-slate-950 rounded-[56px] h-max text-slate-50 p-6 md:p-10 my-10 sm:w-[90%] mx-auto"
+        id="music"
+      >
+        <h2 className="text-3xl lg:text-4xl pb-5 pl-6 sm:px-16 font-bold">
+          {'//Music'}
+        </h2>
+        <div className="flex flex-col items-start justify-center h-full">
+          <p className=" sm:px-12 py-5">
+           this is music list
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolor
+            rem excepturi, est rerum itaque velit dignissimos deleniti facilis eos
+            non, nulla aut corrupti assumenda provident laborum voluptas sed
+            eligendi?
+          </p>
+        </div>
+      </section>
+    );
+  }
+  
